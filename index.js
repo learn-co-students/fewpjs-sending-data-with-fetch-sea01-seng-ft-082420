7b8594c81e93dc17fda650a3b337a8fe4c16084f
jasperwreed <jmixon.reed@gmail.com>
2020-10-07 11:36:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+const submitData = (userName, userEmail) => {
+    const formData = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then( resp => resp.json() )
+    .then( json => document.body.innerHTML = json['id'])
+    .catch( error => document.body.innerHTML = error.message )
+}
